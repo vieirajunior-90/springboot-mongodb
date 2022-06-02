f5245865e1bfc8f66e7a97aa508404dc40dce584
Junior Vieira <junior.vieira.1990@gmail.com>
2022-06-02 13:19:03 -0300

    Project created

---
+++ src/main/java/com/vieira/springmongo/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.vieira.springmongo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+}
---
+++ src/test/java/com/vieira/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
