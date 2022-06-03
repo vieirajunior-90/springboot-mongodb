a9b130d0e78499c5fc3d88e0e7481a4f5db77b13
Junior Vieira <junior.vieira.1990@gmail.com>
2022-06-03 13:17:04 -0300

    Method GET | @Query with regex

--- src/main/java/com/vieira/springmongo/controllers/util/URL.java
+++ src/main/java/com/vieira/springmongo/controllers/util/URL.java
@@ -2,6 +2,9 @@ package com.vieira.springmongo.controllers.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -10,6 +13,17 @@ public class URL {
             return URLDecoder.decode(text, StandardCharsets.UTF_8);
         } catch (Exception e) {
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return sdf.parse(textDate);
+        } catch (Exception e) {
+            return defaultValue;
         }
     }
 }
--- src/main/java/com/vieira/springmongo/services/PostService.java
+++ src/main/java/com/vieira/springmongo/services/PostService.java
@@ -33,4 +33,8 @@ public class PostService {
     public List<Post> findByTitleContainingIgnoreCase(String text) {
         return postRepository.searchTitle(text);
     }
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
+    }
 }
